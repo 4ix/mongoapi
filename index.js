4b35a44cb2bf48c51e5988e2468b6561fb8f93bc
john <jinjihoon@gmail.com>
2020-10-05 12:19:42 +0900

    second commit

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
+const { request } = require("express");
 const express = require("express");
+const mongoose = require("mongoose");
+
 const app = express();
 
 const port = 8080;
 
+app.use(
+  express.urlencoded({
+    extended: false,
+  })
+);
+mongoose.connect(
+  "mongodb+srv://root:1234@cluster0.gzelk.mongodb.net/mydb?retryWrites=true&w=majority",
+  { useNewUrlParser: true, useUnifiedTopology: true }
+);
+
 app.get("/", function (request, response) {
   //   console.log(request.IncomingMessage);
-  response.send("hello, world");
+  response.send("hello, world!");
 });
 
 app.listen(port, function () {
